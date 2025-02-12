8b5182f3cc4da5920289fa388084451a82e84022
Black21396 <fadi.199620@gmail.com>
2025-02-12 07:44:17 +0100

    add update and delete Services

--- src/main/java/net/fadi/student/controller/StudentController.java
+++ src/main/java/net/fadi/student/controller/StudentController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -53,5 +54,51 @@ public class StudentController
     
      return "redirect:/students";
   }
-
+  
+  @GetMapping("/students/edit/{id}")
+  public ModelAndView editStudentForm(@PathVariable("id") Long aId, Model aModel)
+  {
+    Student aStudent = this.studentService.findById(aId);
+    aModel.addAttribute("student", aStudent);
+    
+    return new ModelAndView("edit_student.html");
+  }
+  
+  @PostMapping("/students/edit/{id}")
+  public String editStudent(@PathVariable("id") Long aId, @ModelAttribute("student") Student aStudent)
+  {
+    Student tStudent = this.studentService.findById(aId);
+    
+    if (tStudent != null)
+    {
+      tStudent.setFirstName(aStudent.getFirstName());
+      tStudent.setLastName(aStudent.getLastName());
+      tStudent.setEmail(aStudent.getEmail());
+      
+      this.studentService.saveStudent(aStudent);
+    }
+    
+    return "redirect:/students";
+  }
+  
+  @GetMapping("/students/delete/{id}")
+  public ModelAndView deleteStudentForm(@PathVariable("id") Long aId, Model aModel)
+  {
+    Student tStudent = this.studentService.findById(aId);
+    aModel.addAttribute("student", tStudent);
+    
+    if (tStudent != null)
+      return new ModelAndView("delete_student.html");
+    
+    return null;
+  }
+  
+  @PostMapping("/students/delete/{id}")
+  public String deleteStudent(@PathVariable("id") Long aId)
+  {
+    this.studentService.deleteById(aId);
+    
+    return "redirect:/students";
+  }
+  
 }
--- src/main/java/net/fadi/student/service/StudentService.java
+++ src/main/java/net/fadi/student/service/StudentService.java
@@ -8,6 +8,7 @@
 package net.fadi.student.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -34,6 +35,23 @@ public class StudentService
   public void saveStudent(Student aStudent)
   {
     this.studentRepository.save(aStudent);
+  }
+
+  public Student findById(Long aId)
+  {
+    Optional<Student> tStudent = this.studentRepository.findById(aId);
+    
+    if (tStudent.isPresent())
+      return tStudent.get();
+    return null;
+  }
+
+  public void deleteById(Long aId)
+  {
+    Student tStudent = findById(aId);
+    
+    if (tStudent != null)
+      this.studentRepository.delete(tStudent);
   }
   
 }
